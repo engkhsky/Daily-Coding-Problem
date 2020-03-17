30dadb3c5b608fe343ccfc2b238b258aedf48e3b
engkhsky <engkh.sky@gmail.com>
2020-03-17 14:48:30 +0800

    Create Problem70.java

---
+++ Problem #70_16Mar2020/Problem70.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    int n = readInt("Enter a number: ");
+    int perfectNum = 0, digit = 0;
+    if (n <= 10)
+    {
+      digit = 10 - n;
+      String s = Integer.toString(n) + Integer.toString(digit);
+      perfectNum = Integer.parseInt(s);
+      System.out.println("Result: " + perfectNum); 
+    }
+    else{
+      System.out.println("n is more than 10!");
+    }
+
+    
+  }
+
+  public static int readInt(String prompt) {
+    int input = 0;
+    boolean valid = false;
+    while (!valid) {
+      try {
+        input = Integer.parseInt(readString(prompt));
+        valid = true;
+      } catch (NumberFormatException e) {
+        System.out.println("*** Please enter an integer ***");
+      }
+    }
+    return input;
+  }
+
+  public static String readString(String prompt) {
+    System.out.print(prompt);
+    return new Scanner(System.in).nextLine();
+  }
+}
